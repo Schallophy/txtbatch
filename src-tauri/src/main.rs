fae02f74fcaa68e938b6a41c304a2617ef946946
Schallophy <1317146226@qq.com>
2026-09-16 20:35:50 +0800

    fix: hide console window on Windows release build

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,6 @@
+// Prevents an additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() {
     txtbatch_app_lib::run();
 }
